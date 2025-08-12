d8028dbe7bab34a6f9aa0be4c02a2b05ecf2b147
PoojaLalAU <poojaanija1@gmail.com>
2025-08-12 20:15:54 +0530

    package

---
+++ mypackage/Message.java
@@ -0,0 +1,10 @@
+
+package mypackage;
+
+public class Message {
+    public void display() {
+        System.out.println("Hello from the package!");
+  System.out.println("Lets begin with even no sum");
+      
+    }
+}
